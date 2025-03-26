355a110c30d351fdb873a5b07a3b669b4d3612cd
Enowtabi <enowbibi@gmail.com>
2025-03-26 04:24:18 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div
+      className="flex w-screen h-screen justify-center items-center 
+         text-sm md:text-3xl font-bold bg-gradient-to-r from-amber-400 via-red-500 to-purple-600
+        bg-clip-text text-transparent"
+    >
+      WE CAN WIN THIS THING BROTHER!!
+    </div>
+  )
+}
+
+export default App
